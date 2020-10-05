c5f1b21bae35fc5f3af80f88cd299f5a84a5e768
joao teodoro <joao.teodoro@assert.ifpb.edu.br>
2020-10-05 19:08:00 -0300

    add responsive header

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="header" class="grid-x grid-padding-x align-middle">
+    <div class="cell small-12 text-center position-relative">
+      <h1 class="font-serif">{{ companyName }}</h1>
+      <p>{{ companySlogan }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      companyName: 'Lista de Fundo de Investimentos',
+      companySlogan: 'Conheça a nossa lista de fundos'
+    }
+  }
+}
+</script>
